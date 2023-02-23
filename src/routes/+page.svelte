9b332df71d58502e7ac1b26d1c8f1116de894422
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-02-23 22:15:14 +0000

    create a Navlink component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Navlink from './Navlink.svelte';
+</script>
+
+<nav class="border-green-900 py-2 px-16 flex gap-2 justify-center">
+	<Navlink>About</Navlink>
+	<Navlink>Portfolio</Navlink>
+	<Navlink>Contact</Navlink>
+	<Navlink>Blog</Navlink>
+</nav>
+<div class="flex flex-col items-center">
+	<div class="bg-gray-200 px-8 py-2 rounded-lg">
+		<h1 class="text-lg">Hello 👋, I am a passionate Software developer based in the the UK</h1>
+	</div>
+</div>
---
+++ src/routes/Navlink.svelte
@@ -0,0 +1,1 @@
+<a href="/" class="text-slate-50"><slot /></a>

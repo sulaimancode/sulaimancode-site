babe802fa9245b800d99d92d270b608f6b93cf80
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-02-25 14:59:47 +0000

    remove home icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,9 @@
 	import '../app.css';
 
 	import Navlink from './Navlink.svelte';
-	import Icon from '@iconify/svelte';
 </script>
 
 <nav class="py-4 px-16 flex gap-4 justify-center bg-neutral-600 items-center">
-	<Navlink><Icon icon="mdi-light:home" height="2em" /></Navlink>
 	<Navlink href="/about">About</Navlink>
 	<Navlink>Portfolio</Navlink>
 	<Navlink>Contact</Navlink>

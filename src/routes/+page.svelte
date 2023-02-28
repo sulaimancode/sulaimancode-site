7fb685c5cc27c1c69db298ff1e6f6c8585a60b2a
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-02-28 00:48:18 +0000

    add profile pic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,9 @@
 	<div class="bg-gray-200 px-8 py-2 rounded-lg">
 		<h1 class="text-lg">Hello 👋, I am a passionate Software developer based in the the UK</h1>
 	</div>
+	<img
+		src="/sulai-pic-sm.jpg"
+		alt="Sulai's profile"
+		class="rounded-full w-32 h-32 mt-4 object-cover"
+	/>
 </div>

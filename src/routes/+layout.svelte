ea7c034fd0cad35dbcf3e2a56673ad0a3a07f5d7
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-02-28 23:11:33 +0000

    Make picture smaller, use margin instead of padding, make navbar w-full

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,10 @@
 	import Navlink from './Navlink.svelte';
 </script>
 
-<div class="flex flex-col items-center p-6">
-	<nav class="flex justify-center items-center gap-4 py-4 px-16 backdrop-blur-sm sticky top-0">
+<div class="flex flex-col items-center mx-6">
+	<nav
+		class="flex justify-center items-center w-full gap-4 py-4 px-16 backdrop-blur-sm sticky top-0"
+	>
 		<Navlink>Home</Navlink>
 		<Navlink>Skills</Navlink>
 		<Navlink>Experience</Navlink>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <div class="flex flex-col gap-6 items-center justify-center sm:flex-row">
-	<img src="/sulai-pic-sm.jpg" alt="Sulai's profile" class="rounded-full w-40 h-40 object-cover" />
+	<img src="/sulai-pic-sm.jpg" alt="Sulai's profile" class="rounded-full w-32 h-32 object-cover" />
 	<h1 class="text-lg">Hello 👋, I am a passionate Software developer based in the the UK</h1>
 </div>

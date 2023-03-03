7a9343620b2dbb306bf29436307427e418b76176
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-03-03 23:50:03 +0000

    sort tailwindcss classes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
 	import Navlink from './Navlink.svelte';
 </script>
 
-<div class="flex flex-col items-center h-full">
+<div class="flex h-full flex-col items-center">
 	<nav
-		class="flex justify-center items-center w-full gap-4 py-4 px-16 backdrop-blur-sm sticky top-0"
+		class="sticky top-0 flex w-full items-center justify-center gap-4 py-4 px-16 backdrop-blur-sm"
 	>
 		<Navlink>Home</Navlink>
 		<Navlink>Skills</Navlink>
@@ -12,14 +12,14 @@
 		<Navlink>Contact</Navlink>
 	</nav>
 
-	<div class="flex flex-col gap-6 items-center justify-center my-auto sm:flex-row">
+	<div class="my-auto flex flex-col items-center justify-center gap-6 sm:flex-row">
 		<img
 			src="/sulai-pic-sm.jpg"
 			alt="Sulai's profile"
-			class="rounded-full w-32 h-32 object-cover"
+			class="h-32 w-32 rounded-full object-cover"
 		/>
-		<div class="bg-neutral-800 p-4 rounded-md w-[85%] sm:w-[60%]">
-			<p>
+		<div class="w-[85%] rounded-md bg-neutral-800 p-4 sm:w-[60%]">
+			<p  >
 				Hello 👋 I am Sulaiman, a software engineer from the UK. I enjoy creating and building stuff
 				with code. Currently working at <a
 					class="text-blue-200 hover:underline"

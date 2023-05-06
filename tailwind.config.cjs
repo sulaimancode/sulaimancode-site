e1308942f19669a0176dd5e73879658e95503a34
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-05-06 19:22:08 +0100

    use Open Sans font and and an animation for the waving hand

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,26 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans]
+			},
+			keyframes: {
+				'wave-animation': {
+					'0%': { transform: 'rotate(0.0deg)' },
+					'10%': { transform: 'rotate(14.0deg)' },
+					'20%': { transform: 'rotate(-8.0deg)' },
+					'30%': { transform: 'rotate(14.0deg)' },
+					'40%': { transform: 'rotate(-4.0deg)' },
+					'50%': { transform: 'rotate(10.0deg)' },
+					'60%': { transform: 'rotate(0.0deg)' },
+					'100%': { transform: 'rotate(0.0deg)' }
+				}
+			},
+			animation: {
+				'wave-animation': 'wave-animation 2.5s infinite'
+			}
+		}
 	},
 	plugins: []
 };

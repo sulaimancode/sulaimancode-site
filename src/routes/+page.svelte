fcde78fb4d94f86bba9c370b2cfda2ec93e3ea12
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-05-08 14:44:28 +0100

    Fix size of svg text and an animate prop to animate on click

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 	import Navlink from './Navlink.svelte';
 	import ListItem from './ListItem.svelte';
 	import Skill from './Skill.svelte';
+
+	let animate = false;
 </script>
 
 <nav class="sticky top-0 flex w-full items-center justify-center gap-4 py-4 px-16 backdrop-blur-sm">
@@ -11,7 +13,7 @@
 	<Navlink>Contact</Navlink>
 </nav>
 
-<div class="mx-auto w-[45%]">
+<div class="mx-auto w-[60%]">
 	<div class="py-40">
 		<h1 class="text-center text-3xl font-bold">
 			Hello <span class="inline-block origin-[70%_70%] animate-wave-animation">👋</span>
@@ -52,12 +54,18 @@
 
 	<article>
 		<h2 class="mb-2 text-xl font-bold">Skills</h2>
-		<Skill name="Typescript" level={0.9} />
-		<Skill name="React" level={0.8} />
-		<Skill name="Redux" level={0.7} />
-		<Skill name="SvelteKit" level={0.25} />
-		<Skill name="Ruby" level={0.5} />
-
+		<div class="flex flex-wrap gap-4">
+			<Skill name="Typescript" level={0.9} {animate} />
+			<Skill name="React" level={0.8} {animate} />
+			<Skill name="Redux" level={0.7} {animate} />
+			<Skill name="SvelteKit" level={0.25} {animate} />
+			<Skill name="Ruby" level={0.5} {animate} />
+		</div>
+		<button
+			on:click={() => {
+				animate = true;
+			}}>click to animate</button
+		>
 		<h1>Other stuff I've used:</h1>
 		<ul>
 			<ListItem>C#</ListItem>
--- src/routes/Skill.svelte
+++ src/routes/Skill.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	export let name: string;
 	export let level: number;
+	export let animate: boolean;
 
 	const red = '#ff4136';
 	const orange = '#ff851b';
 	const green = '#2ecc40';
+	const fontSize = 30;
 
 	let animateValues = `${red}; ${red}; ${orange}; ${orange}; ${green}`;
 	let animateKeyTimes = '0; 0.4; 0.5; 0.55; 1';
@@ -19,23 +21,38 @@
 		animateKeyTimes = '0; 1';
 	}
 
+	let svg: SVGSVGElement | undefined;
 	let text1: SVGTextElement | undefined;
 
 	let textWidth: number | undefined;
 	let duration: number | undefined;
+	let svgWidth: number = 0;
 
-	$: if (text1) {
+	$: if (svg && text1) {
 		textWidth = text1.getComputedTextLength();
-		duration = textWidth / 200;
+		duration = textWidth / 100;
+
+		const bbox = svg.getBBox();
+		svgWidth = bbox.x + bbox.width + bbox.x;
+	}
+
+	let animateWidth: SVGAnimateElement | undefined;
+	let animateFill: SVGAnimateElement | undefined;
+
+	$: if (animate && animateWidth && animateFill) {
+		animateWidth.beginElement();
+		animateFill.beginElement();
 	}
 </script>
 
-<svg width="100%" height="100%" viewBox="0 0 800 200">
+<svg bind:this={svg} width={svgWidth} height={40}>
 	<defs>
-		<pattern id={`pattern-${name}`} patternUnits="userSpaceOnUse" width="800" height="200">
+		<pattern id={`pattern-${name}`} patternUnits="userSpaceOnUse" width="210" height="40">
 			{#if textWidth && duration}
-				<rect x="0" y="0" height="200" width="0" fill="#ff4136">
+				<rect x="0" y="0" height="40" width="0" fill="#ff4136">
 					<animate
+						bind:this={animateWidth}
+						begin="indefinite"
 						attributeName="width"
 						from="0"
 						to={textWidth * level}
@@ -44,6 +61,8 @@
 					/>
 
 					<animate
+						bind:this={animateFill}
+						begin="indefinite"
 						attributeName="fill"
 						values={animateValues}
 						keyTimes={animateKeyTimes}
@@ -57,17 +76,17 @@
 	<text
 		bind:this={text1}
 		x="0"
-		y="150"
+		y="25"
 		font-family="sans-serif"
-		font-size="120"
+		font-size={fontSize}
 		font-weight="900"
 		class="stroke-white stroke-1">{name}</text
 	>
 	<text
 		x="0"
-		y="150"
+		y="25"
 		font-family="sans-serif"
-		font-size="120"
+		font-size={fontSize}
 		font-weight="900"
 		fill={`url(#pattern-${name})`}
 		class="stroke-white stroke-1">{name}</text

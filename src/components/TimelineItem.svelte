b123aef184fb3c2fd9badb4ba164940274fd1040
Slaiman Ahmadshah <slaiman.ahmadshah@red-gate.com>
2023-05-24 21:41:47 +0100

    change font colors for better contrast

--- src/components/TimelineItem.svelte
+++ src/components/TimelineItem.svelte
@@ -8,7 +8,7 @@
 	<div
 		class="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"
 	/>
-	<time class="mb-1 text-sm font-normal leading-none text-gray-500">{date}</time>
+	<time class="mb-1 text-sm font-normal leading-none text-gray-400">{date}</time>
 	<h3 class="text-lg font-semibold">{company}</h3>
 	<p class="mb-4 text-base font-normal text-slate-300">{description}</p>
 </li>
